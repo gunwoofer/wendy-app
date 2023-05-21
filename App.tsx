9dbd6fa6a9c40d02bfaa6ab72d070f855447eaf4
Valentin <valentin.bouis@gmail.com>
2023-05-20 22:38:00 -0400

    intial

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/components/login/login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Details" component={Login} />
+        <Stack.Screen name="Home" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/components/login/login.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text>Login component working</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
