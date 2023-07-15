361b21868fd44692857283dbaf04c27e36c9f02b
Valentin <valentin.bouis@gmail.com>
2023-07-15 16:29:10 -0400

    tabs

--- src/screens/WeekendScreen/WeekendScreen.tsx
+++ src/screens/WeekendScreen/WeekendScreen.tsx
@@ -4,7 +4,8 @@ import { Weekend } from '../../models/weekend';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../../navigation/userStack';
 import { RouteProp } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialIcons, MaterialCommunityIcons   } from '@expo/vector-icons';
+import { styles } from './WeekendScreenStyle';
 
 type WeekendScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Weekend'>;
 type WeekendProps = {
@@ -24,27 +25,28 @@ const WeekendScreen = ({route, navigation}: WeekendProps) => {
 
       {/* Tabs */}
       <View style={styles.content}>
-        {/* My Weekends Tab */}
+        {/* Weekend info */}
         <TouchableOpacity
           style={[styles.tabButton]}
         >
           <Ionicons name="calendar-outline" size={24} color={'#000'} />
-          <Text style={[styles.tabText, styles.activeTabText]}>My Weekends</Text>
+          <Text style={[styles.tabText, styles.activeTabText]}>General</Text>
         </TouchableOpacity>
 
-        {/* Profile Tab */}
+        {/* Transport */}
         <TouchableOpacity
           style={[styles.tabButton]}
         >
-          <Ionicons name="person-outline" size={24} color={'#000'} />
-          <Text style={[styles.tabText, styles.activeTabText]}>Profile</Text>
+          <Ionicons name="car" size={24} color={'#000'} />
+          <Text style={[styles.tabText, styles.activeTabText]}>Transport</Text>
         </TouchableOpacity>
 
+        {/* Participants */}
         <TouchableOpacity
           style={[styles.tabButton]}
         >
-          <Ionicons name="person-outline" size={24} color={'#000'} />
-          <Text style={[styles.tabText, styles.activeTabText]}>Info</Text>
+          <MaterialCommunityIcons name="account-group" size={24} color={'#000'} />
+          <Text style={[styles.tabText, styles.activeTabText]}>Guests</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -53,80 +55,3 @@ const WeekendScreen = ({route, navigation}: WeekendProps) => {
 
 export default WeekendScreen;
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#F5F5F5',
-      alignItems: 'center',
-      justifyContent: 'flex-end',
-    },
-    content: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      width: '100%',
-      paddingHorizontal: 40,
-      paddingVertical: 8,
-      backgroundColor: '#FFF',
-      elevation: 2,
-    },
-    tabButton: {
-      alignItems: 'center',
-    },
-    tabText: {
-      fontSize: 12,
-      marginTop: 4,
-    },
-    activeTabText: {
-      color: '#007AFF',
-    },
-    addButton: {
-      position: 'absolute',
-      bottom: 16,
-      alignSelf: 'center',
-      backgroundColor: '#007AFF',
-      borderRadius: 100,
-      padding: 16,
-      elevation: 4,
-    },
-    modalContainer: {
-      flex: 1,
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    modalContent: {
-      backgroundColor: '#FFF',
-      padding: 20,
-      borderRadius: 8,
-      width: '80%',
-    },
-    modalTitle: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      marginBottom: 16,
-    },
-    input: {
-      borderWidth: 1,
-      borderColor: '#CCC',
-      borderRadius: 4,
-      padding: 8,
-      marginBottom: 16,
-    },
-    modalButtonContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    responseContainer: {
-        bottom: '30%'
-    },
-    profileContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    profileEmail: {
-    fontSize: 16,
-    marginBottom: 16,
-    },
-  });
