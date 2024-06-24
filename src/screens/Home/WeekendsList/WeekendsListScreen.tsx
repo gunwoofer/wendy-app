000f3905da6835650b8c27e00e0077c8c13da353
Valentin <valentin.bouis@gmail.com>
2024-06-23 23:06:32 -0400

    refactor weekendList with service

--- src/screens/Home/WeekendsList/WeekendsListScreen.tsx
+++ src/screens/Home/WeekendsList/WeekendsListScreen.tsx
@@ -1,27 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Button, FlatList, Modal, TextInput, TouchableOpacity, Alert, RefreshControl } from 'react-native';
-import { styles } from './WeekendsListScreenStyle';
-import WeekendCard from '../../../components/WeekendCard/WeekendCard';
-import { useAuthentication } from '../../../utils/hooks/useAuthentification';
-import { Weekend } from '../../../models/weekend';
-import { RouteProp } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { HomeStackParamList } from '../Home';
-import { Ionicons, FontAwesome } from '@expo/vector-icons';
-import * as RootNavigation from '../../../navigation/RootNavigation';
-import { useFocusEffect } from '@react-navigation/native';
-import { EXPO_PUBLIC_SERVER_IP } from '@env'
-import { useStoreActions, useStoreState } from '../../../state/hooks';
-import WeekendService from '../../../services/WeekendService';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  FlatList,
+  Modal,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  RefreshControl,
+} from "react-native";
+import { styles } from "./WeekendsListScreenStyle";
+import WeekendCard from "../../../components/WeekendCard/WeekendCard";
+import { useAuthentication } from "../../../utils/hooks/useAuthentification";
+import { Weekend } from "../../../models/weekend";
+import { RouteProp } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { HomeStackParamList } from "../Home";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import * as RootNavigation from "../../../navigation/RootNavigation";
+import { useFocusEffect } from "@react-navigation/native";
+import { EXPO_PUBLIC_SERVER_IP } from "@env";
+import { useStoreActions, useStoreState } from "../../../state/hooks";
+import WeekendService from "../../../services/WeekendService";
 
-
-
-type WeekendsListScreenNavigationProp = StackNavigationProp<HomeStackParamList, 'WeekendsList'>;
+type WeekendsListScreenNavigationProp = StackNavigationProp<
+  HomeStackParamList,
+  "WeekendsList"
+>;
 type WeekendsListProps = {
   navigation: WeekendsListScreenNavigationProp;
-  route: RouteProp<HomeStackParamList, 'WeekendsList'>;
+  route: RouteProp<HomeStackParamList, "WeekendsList">;
 };
-
 
 const WeekendsListScreen = ({ route, navigation }: WeekendsListProps) => {
   const weekendService = WeekendService.getInstance();
@@ -32,45 +43,44 @@ const WeekendsListScreen = ({ route, navigation }: WeekendsListProps) => {
   const [myWeekends, setmyWeekends] = useState<Weekend[]>([]);
   const [refreshing, setRefreshing] = useState(false);
 
-  const [futureWeekends, setFutureWeekends] = useState<Weekend[]>([]);
-  const [pastWeekends, setPastWeekends] = useState<Weekend[]>([]);
-
   const [allWeekends, setAllWeekends] = useState<Weekend[]>([]);
 
   useFocusEffect(
     React.useCallback(() => {
       if (user) {
-        getWeekends()
+        getWeekends();
       }
     }, [user])
   );
 
   useEffect(() => {
-
-    if (user)
-      getWeekends().catch(console.error);
+    if (user) {
+      getWeekends();
+    }
   }, [user]);
 
+  useEffect(() => {
+    categorizeWeekends(myWeekends);
+  }, [myWeekends]);
 
   const onRefresh = () => {
     if (user) {
-      setRefreshing(true);
       getWeekends();
     }
-  }
+  };
 
   const categorizeWeekends = (weekends: Weekend[]) => {
     const currentDate = new Date();
     const categorizedWeekends = weekends.map((weekend) => ({
       ...weekend,
-      status: new Date(weekend.date_debut) > currentDate || weekend.date_debut == undefined ? "coming" : "past",
+      status:
+        new Date(weekend.date_debut) > currentDate ||
+        weekend.date_debut == undefined
+          ? "coming"
+          : "past",
     }));
     setAllWeekends(categorizedWeekends);
   };
-
-  useEffect(() => {
-    categorizeWeekends(myWeekends);
-  }, [myWeekends]);
 
   // Sorting function to sort weekends by date
   const sortByDate = (a: Weekend, b: Weekend) => {
@@ -81,38 +91,27 @@ const WeekendsListScreen = ({ route, navigation }: WeekendsListProps) => {
     } else if (!a.date_debut && !b.date_debut) {
       return 0; // No sorting needed for weekends with undefined date_debut
     }
-  
+
     return new Date(b.date_debut).getTime() - new Date(a.date_debut).getTime();
-  
   };
-
-
 
   const getWeekends = async () => {
-    console.log("TEST SERVER IP : ", EXPO_PUBLIC_SERVER_IP)
-    const response = await fetch(EXPO_PUBLIC_SERVER_IP + '/getWeekends', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ user_id: user!.uid }),
+    console.log("receiving weekends... turning refresh true");
+    setRefreshing(true);
+    weekendService.getWeekends(user!.uid).then((weekends: Weekend[]) => {
+      setmyWeekends(weekends);
+      console.log("received weekends, turning refresh false");
+      setRefreshing(false);
     });
-    console.log("getWeekends response")
-    const data = await response.json();
-    
-    console.log("getWeekends data")
-    console.log(data)
-    setmyWeekends(data);
-    setRefreshing(false);
-  }
-
-
-  const handleCardPress = async (weekend: Weekend) => {
-    const chosenWeekend: Weekend = await weekendService.getWeekendByIdAPI(weekend.id);
-    setCurrentWeekend(chosenWeekend);
-    RootNavigation.navigate('Weekend')
   };
 
+  const handleCardPress = async (weekend: Weekend) => {
+    const chosenWeekend: Weekend = await weekendService.getWeekendByIdAPI(
+      weekend.id
+    );
+    setCurrentWeekend(chosenWeekend);
+    RootNavigation.navigate("Weekend");
+  };
 
   return (
     <View style={styles.container}>
@@ -120,13 +119,13 @@ const WeekendsListScreen = ({ route, navigation }: WeekendsListProps) => {
       {allWeekends.length > 0 && (
         <FlatList
           data={allWeekends.sort(sortByDate)}
-          renderItem={({ item, index }: {item: any, index: number}) => (
+          renderItem={({ item, index }: { item: any; index: number }) => (
             <View>
               {index === 0 || allWeekends[index - 1].status !== item.status ? (
                 <View style={styles.sectionLabelContainer}>
                   <View style={styles.sectionSeparator} />
                   <Text style={styles.sectionLabelText}>
-                    {item.status === 'coming' ? 'Coming' : 'Past'}
+                    {item.status === "coming" ? "Coming" : "Past"}
                   </Text>
                   <View style={styles.sectionSeparator} />
                 </View>
@@ -134,13 +133,14 @@ const WeekendsListScreen = ({ route, navigation }: WeekendsListProps) => {
               <WeekendCard weekend={item} onPress={handleCardPress} />
             </View>
           )}
-          keyExtractor={(item: { id: { toString: () => any; }; }) => item.id.toString()}
+          keyExtractor={(item: { id: { toString: () => any } }) =>
+            item.id.toString()
+          }
           refreshControl={
             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
         />
       )}
-     
     </View>
   );
 };
--- src/services/WeekendService.tsx
+++ src/services/WeekendService.tsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
-import { Weekend } from '../models/weekend';
-import { EXPO_PUBLIC_SERVER_IP } from '@env';
+import React, { useState } from "react";
+import { Weekend } from "../models/weekend";
+import { EXPO_PUBLIC_SERVER_IP } from "@env";
+import { useAuthentication } from "../utils/hooks/useAuthentification";
 
 // Define the type for the WeekendService instance
 interface WeekendServiceInstance {
-  setWeekendAPI: (id: number, name: string, address: string, tricount_link: string, reservation_link: string, date_debut: string, date_fin: string) => Promise<Weekend>;
+  setWeekendAPI: (
+    id: number,
+    name: string,
+    address: string,
+    tricount_link: string,
+    reservation_link: string,
+    date_debut: string,
+    date_fin: string
+  ) => Promise<Weekend>;
   getWeekendByIdAPI: (id: number) => Promise<Weekend>;
   setWeekendPhoto(id: number, image_base64: string): Promise<any>;
+  getWeekends(userId: string): Promise<Weekend[]>;
 }
 
 // Define the type for the singleton instance of WeekendService
@@ -19,105 +29,99 @@ const WeekendService: WeekendServiceSingleton = (function () {
   let instance: WeekendServiceInstance | null = null;
 
   function createInstance(): WeekendServiceInstance {
-
-    async function setWeekendAPI(id: number, name: string, address: string, tricount_link: string, reservation_link: string, date_debut: string, date_fin: string): Promise<Weekend> {
-      console.log("update weekend")
-      const response = await fetch(`${EXPO_PUBLIC_SERVER_IP}/updateWeekend/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          "name": name,
-          "address": address,
-          "tricount_link": tricount_link,
-          "reservation_link": reservation_link,
-          "date_debut": date_debut,
-          "date_fin": date_fin }),
-      });
+    async function setWeekendAPI(
+      id: number,
+      name: string,
+      address: string,
+      tricount_link: string,
+      reservation_link: string,
+      date_debut: string,
+      date_fin: string
+    ): Promise<Weekend> {
+      console.log("update weekend");
+      const response = await fetch(
+        `${EXPO_PUBLIC_SERVER_IP}/updateWeekend/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: name,
+            address: address,
+            tricount_link: tricount_link,
+            reservation_link: reservation_link,
+            date_debut: date_debut,
+            date_fin: date_fin,
+          }),
+        }
+      );
       const weekend: Weekend = await response.json();
       return weekend;
     }
 
     async function getWeekendByIdAPI(id: number): Promise<Weekend> {
-      console.log("get weekend by id")
+      console.log("get weekend by id");
 
-      const response = await fetch(EXPO_PUBLIC_SERVER_IP + '/getWeekendById/' + id, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
+      const response = await fetch(
+        EXPO_PUBLIC_SERVER_IP + "/getWeekendById/" + id,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      });
-      console.log("fetch get weekend by id")
-
+      );
       const weekend: Weekend = await response.json();
-
-      // const weekend: Weekend = {
-      //   id: 1,
-      //   name: "Weekend 1",
-      //   date_debut: "2021-05-01",
-      //   date_fin: "2021-05-02",
-      //   status: "coming",
-      //   address: "address",
-      //   participants: [
-      //     {
-      //       id: 1,
-      //       first_name: "User",
-      //       second_name: "Second",
-      //       email: "user@gmail.com",
-      //       is_present: true,
-      //       is_driver: false
-      //     },
-      //     {
-      //       id: 2,
-      //       first_name: "User2",
-      //       second_name: "Second2",
-      //       email: "user2@gmail.com",
-      //       is_present: false,
-      //       is_driver: false
-      //     },
-      //     {
-      //       id: 3,
-      //       first_name: "User3",
-      //       second_name: "Second3",
-      //       email: "user3@gmail.com",
-      //       is_present: true,
-      //       is_driver: true
-      //     },
-      //   ],
-      //   sharing_code: "sharing_code",
-      //   tricount_link: "tricount_link",
-      //   reservation_link: "reservation_link"
-      // }
-
-      console.log("weekend: get weekend by id")
       return weekend;
     }
 
-    async function setWeekendPhoto(id: number, image_base64: string): Promise<any> {
-      console.log("set weekend photo")
-      const response = await fetch(EXPO_PUBLIC_SERVER_IP + '/upload_image/' + id, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          base64_image: image_base64
-        }),
-      });
+    async function setWeekendPhoto(
+      id: number,
+      image_base64: string
+    ): Promise<any> {
+      console.log("set weekend photo");
+      const response = await fetch(
+        EXPO_PUBLIC_SERVER_IP + "/upload_image/" + id,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            base64_image: image_base64,
+          }),
+        }
+      );
 
       if (!response.ok) {
-        throw new Error('Image upload failed');
+        throw new Error("Image upload failed");
       }
 
       const responseData = await response.json();
       return responseData;
     }
 
+    async function getWeekends(userId: string): Promise<Weekend[]> {
+      const response = await fetch(EXPO_PUBLIC_SERVER_IP + "/getWeekends", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user_id: userId }),
+      });
+      if (!response.ok) {
+        throw new Error("Error getting the weekends list");
+      }
+      const data = await response.json();
+      return data;
+    }
+
     return {
       setWeekendAPI,
       getWeekendByIdAPI,
-      setWeekendPhoto
+      setWeekendPhoto,
+      getWeekends,
     };
   }
 
