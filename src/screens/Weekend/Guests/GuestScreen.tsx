728e8fa53eb87b3ce3228a1d5e4d551eb2bbd63d
Valentin <valentin.bouis@gmail.com>
2024-06-24 15:31:04 -0400

    remove custom navigation bar

--- src/screens/Weekend/Guests/GuestScreen.tsx
+++ src/screens/Weekend/Guests/GuestScreen.tsx
@@ -1,35 +1,39 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList, RefreshControl } from 'react-native';
-import { styles } from './GuestScreenStyle';
-import UserCard from '../../../components/UserCard/UserCard';
-import { RouteProp } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { WeekendStackParamList } from '../WeekendScreen';
-import { useStoreState, useStoreActions } from '../../../state/hooks';
-import WeekendService from '../../../services/WeekendService';
-import { Weekend } from '../../../models/weekend';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, FlatList, RefreshControl } from "react-native";
+import { styles } from "./GuestScreenStyle";
+import UserCard from "../../../components/UserCard/UserCard";
+import { RouteProp } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { WeekendStackParamList } from "../WeekendScreen";
+import { useStoreState, useStoreActions } from "../../../state/hooks";
+import WeekendService from "../../../services/WeekendService";
+import { Weekend } from "../../../models/weekend";
 
-type GuestsScreenNavigationProp = StackNavigationProp<WeekendStackParamList, 'Guest'>;
+type GuestsScreenNavigationProp = StackNavigationProp<
+  WeekendStackParamList,
+  "Guest"
+>;
 type GuestsProps = {
-    navigation: GuestsScreenNavigationProp;
-    route: RouteProp<WeekendStackParamList, 'Guest'>;
+  navigation: GuestsScreenNavigationProp;
+  route: RouteProp<WeekendStackParamList, "Guest">;
 };
 
-
-const GuestsScreen = ({route, navigation}: GuestsProps) => {
-
+const GuestsScreen = ({ route, navigation }: GuestsProps) => {
   const weekendService = WeekendService.getInstance();
-  const currentWeekend = useStoreState(state => state.currentWeekend);
+  const currentWeekend = useStoreState((state) => state.currentWeekend);
   const setCurrentWeekend = useStoreActions((actions) => actions.setWeekend);
 
   const [isRefreshing, setIsRefreshing] = useState(false);
-  
+
   const handleRefresh = async () => {
     setIsRefreshing(true);
-    const updatedWeekend: Weekend = await weekendService.getWeekendByIdAPI(currentWeekend!.id);
+    const updatedWeekend: Weekend = await weekendService.getWeekendByIdAPI(
+      currentWeekend!.id
+    );
     console.log("REFRESHED SUCCESSFULL : ", updatedWeekend);
     setCurrentWeekend(updatedWeekend);
     setIsRefreshing(false);
+    console.log("participants : ", currentWeekend?.participants);
   };
 
   return (
@@ -37,13 +41,17 @@ const GuestsScreen = ({route, navigation}: GuestsProps) => {
       <Text style={styles.text}>Guests</Text>
       <Text>{currentWeekend!.name}</Text>
       <FlatList
-            // TODO: Se mettre en priorite dans le tri en utilisant authentification pour la comparaison
-            data={currentWeekend!.participants.sort((a, b) => a.first_name.localeCompare(b.second_name))}
-            renderItem={({ item }) => (
-              <UserCard user={item} weekend={currentWeekend!}/>
-            )}
-            refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh}/>}
-          />
+        // TODO: Se mettre en priorite dans le tri en utilisant authentification pour la comparaison
+        data={currentWeekend!.participants.sort((a, b) =>
+          a.first_name.localeCompare(b.second_name)
+        )}
+        renderItem={({ item }) => (
+          <UserCard user={item} weekend={currentWeekend!} />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />
+        }
+      />
       <Text>date: {currentWeekend!.date_debut}</Text>
       <Text>Sharing code: {currentWeekend!.sharing_code}</Text>
     </View>
