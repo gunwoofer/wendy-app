c23ace4e99fd5b5c896f8642896d80b504f5c30c
Valentin <valentin.bouis@gmail.com>
2025-01-04 16:43:46 -0500

    sauvegarde de la participation de l utilisateur

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,52 +1,55 @@
-import { EXPO_PUBLIC_SERVER_IP } from '@env';
-import React, { useState } from 'react';
-import {Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { EXPO_PUBLIC_SERVER_IP } from "@env";
+import React, { useState } from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
-import { UserModel } from '../../models/user';
-import { Weekend } from '../../models/weekend';
-import { User } from 'firebase/auth';
-import { useAuthentication } from '../../utils/hooks/useAuthentification';
+import { UserModel } from "../../models/user";
+import { Weekend } from "../../models/weekend";
+import { User } from "firebase/auth";
+import { useAuthentication } from "../../utils/hooks/useAuthentification";
 
-
-
-const UserCard = (props: {user: UserModel, weekend: Weekend}) => {
+const UserCard = (props: { user: UserModel; weekend: Weekend }) => {
   // const {user} = useAuthentication();
   const [checked, setChecked] = useState(props.user.is_present);
 
-
-  const update_is_present = (user: UserModel, weekend: Weekend) => {
-    const func = async (isChecked: boolean) => {
-      // const response = await fetch(`${EXPO_PUBLIC_SERVER_IP}/updateWeekendPresence/${weekend.id}`, {
-      //   method: 'PUT',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify({
-      //     "user_id": user.id,
-      //     "is_present": isChecked
-      //   }),
-      // });
-      user.is_present = !user.is_present
-      setChecked(!checked)
-    };
-    return func
+  const update_is_present = async (user: UserModel, weekend: Weekend) => {
+    const response = await fetch(
+      `${EXPO_PUBLIC_SERVER_IP}/updateWeekendPresence/${weekend.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_id: user.id,
+          is_present: !checked,
+        }),
+      }
+    );
+    user.is_present = !user.is_present;
+    setChecked(!checked);
   };
 
   return (
-      <BouncyCheckbox
-        // disabled={props.weekend.creator != props.user.id}
-        text={props.user.first_name + " " + props.user.second_name}
-        isChecked={checked}
-        onPress={update_is_present(props.user, props.weekend)}
-        style={styles.card}
-        textStyle={checked? {textDecorationLine: "none",}: {textDecorationLine: "line-through",}}
-      />
+    <BouncyCheckbox
+      // disabled={props.weekend.creator != props.user.id}
+      text={props.user.first_name + " " + props.user.second_name}
+      isChecked={checked}
+      onPress={(isChecked: boolean) =>
+        update_is_present(props.user, props.weekend)
+      }
+      style={styles.card}
+      textStyle={
+        checked
+          ? { textDecorationLine: "none" }
+          : { textDecorationLine: "line-through" }
+      }
+    />
   );
 };
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 8,
     padding: 16,
     marginVertical: 8,
@@ -54,7 +57,7 @@ const styles = StyleSheet.create({
   },
   cardTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
